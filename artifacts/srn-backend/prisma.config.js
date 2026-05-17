18142f1f30cd8056f2d779fafc7cae38f74174e6
Joohhnnyyy <johnsonansh32@gmail.com>
2026-05-17 20:00:43 +0530

    fix: convert prisma config to commonjs and copy it in docker stages for deployment

---
+++ artifacts/srn-backend/prisma.config.js
@@ -0,0 +1,13 @@
+// This file is configured for Prisma 7 compatibility in both Development and Production stages.
+require("dotenv/config");
+const { defineConfig } = require("prisma/config");
+
+module.exports = defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
